853f7900b7cc9863f889da2cf30133062e3c69ef
lamia bn <lamiabane65@gmail.com>
2025-12-17 00:42:39 +0100

    l'affichage les services dynamiquement à l'aide de php

--- views/services.php
+++ views/services.php
@@ -1,31 +1,28 @@
 <?php
 require_once __DIR__ . "/../templates/header.php";
 ?>
+<?php
 
+$jsonData = file_get_contents(__DIR__ . '/../data/services.json');
+$services = json_decode($jsonData, true);
+
+?>
 <section>
   <h2 class="text-3xl font-bold mb-10 text-center">Nos services</h2>
 
   <div class="grid md:grid-cols-3 gap-8">
-    <div class="bg-white p-6 rounded-lg shadow">
-      <h3 class="text-xl font-semibold mb-2">Design UI/UX</h3>
-      <p class="text-gray-600">
-        Conception d’interfaces modernes et intuitives centrées sur l’utilisateur.
-      </p>
-    </div>
 
-    <div class="bg-white p-6 rounded-lg shadow">
-      <h3 class="text-xl font-semibold mb-2">Identité visuelle</h3>
-      <p class="text-gray-600">
-        Création de logos et chartes graphiques professionnelles.
-      </p>
-    </div>
+    <?php foreach ($services as $service): ?>
+      <div class="bg-white p-6 rounded-lg shadow">
+        <h3 class="text-xl font-semibold mb-2">
+          <?= htmlspecialchars($service["title"]) ?>
+        </h3>
+        <p class="text-gray-600">
+          <?= htmlspecialchars($service["description"]) ?>
+        </p>
+      </div>
+    <?php endforeach; ?>
 
-    <div class="bg-white p-6 rounded-lg shadow">
-      <h3 class="text-xl font-semibold mb-2">Web Design</h3>
-      <p class="text-gray-600">
-        Maquettes modernes adaptées aux besoins de votre entreprise.
-      </p>
-    </div>
   </div>
 </section>
 
